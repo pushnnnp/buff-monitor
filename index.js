28c9c4a9cd68863bec4794693cf7ff94be5114ed
pushnnnp <157004819+pushnnnp@users.noreply.github.com>
2025-11-20 00:21:40 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -88,4 +88,5 @@ async function monitor() {
 }
 
 monitor();
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
